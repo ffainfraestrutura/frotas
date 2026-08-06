6c649095193cd13045afc443e47572aa3bd32731
Desenvolvimento2FFA <suportedesenvolvimento@ffainfraestrutura.com.br>
2026-08-06 17:41:08 +0000

    feat: atualiza menu superior com linksaldo semanal

--- includes/menu_superior_simples.php
+++ includes/menu_superior_simples.php
@@ -283,6 +283,21 @@ function menuSuperiorAtivo(string $caminho, string $paginaAtual, string $baseAut
                             </li>
                         </ul>
                     </li>
+
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="afMenuGestaoPerfil4" role="button"
+                            data-bs-toggle="dropdown" aria-expanded="false">
+                            <i class="fas fa-briefcase me-1"></i>Gestão
+                        </a>
+                        <ul class="dropdown-menu" aria-labelledby="afMenuGestaoPerfil4">
+                            <li>
+                                <a class="dropdown-item" href="<?= menuSuperiorLink('cotafixa.php', $baseAutofrotaUrl) ?>" target="_self">Cota Fixa</a>
+                            </li>
+                            <li>
+                                <a class="dropdown-item" href="<?= menuSuperiorLink('relatorio-saldo-veiculos.php', $baseAutofrotaUrl) ?>" target="_self">Relatório de Saldo</a>
+                            </li>
+                        </ul>
+                    </li>
                 <?php endif; ?>
 
                 <?php if ($_SESSION['perfil'] == 4 || $_SESSION['perfil'] == 12): ?>
