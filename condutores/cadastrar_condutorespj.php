82ca3dbbef70da9408963a18a3a5cbeedf3240a9
Desenvolvimento2FFA <suportedesenvolvimento@ffainfraestrutura.com.br>
2026-08-24 17:41:13 +0000

    feat: atualiza pra usar combo e nao input de texto pra cc

--- condutores/cadastrar_condutorespj.php
+++ condutores/cadastrar_condutorespj.php
@@ -64,7 +64,7 @@ function renderizarOpcoesCentroCusto(array $linhas): void
     }
 }
 
-renderCabecalhoAutofrota('Cadastrar Condutor');
+renderCabecalhoAutofrota('Cadastrar Condutor PJ');
 ?>
 <style>
     .btn-collapse {
@@ -273,8 +273,10 @@ renderCabecalhoAutofrota('Cadastrar Condutor');
                             </div>
                             <div class="col-md-3">
                                 <label for="ccusto" class="form-label">Departamento/Centro de Custo:<span class="text-danger">*</span></label>
-                                <input class="form-control form-control-sm text-uppercase" name="ccusto" id="ccusto" list="listaCcusto" required>
-                                <datalist id="listaCcusto"><?php renderizarOpcoesCentroCusto($ccustos['linhas']); ?></datalist>
+                                <select class="form-select form-select-sm" name="ccusto" id="ccusto" required>
+                                    <option value="">Selecione</option>
+                                    <?php renderizarOpcoesCentroCusto($ccustos['linhas']); ?>
+                                </select>
                             </div>
                             <div class="col-md-3">
                                 <label for="cargo" class="form-label">Cargo:<span class="text-danger">*</span></label>
