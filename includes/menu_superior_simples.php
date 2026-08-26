45be1876bf5e6d33e958b8d5c84f6611a9de5df7
Desenvolvimento2FFA <suportedesenvolvimento@ffainfraestrutura.com.br>
2026-08-26 17:45:46 +0000

    refactor: oculta opção de colaboradores com cnh

--- includes/menu_superior_simples.php
+++ includes/menu_superior_simples.php
@@ -253,9 +253,9 @@ function menuSuperiorAtivo(string $caminho, string $paginaAtual, string $baseAut
                             <li><a class="dropdown-item"
                                     href="<?= menuSuperiorLink('condutores/listagem-condutor.php', $baseAutofrotaUrl) ?>"
                                     target="_self">Listagem geral de condutores com veículos</a></li>
-                            <li><a class="dropdown-item"
+                            <!-- <li><a class="dropdown-item"
                                     href="<?= menuSuperiorLink('condutores/listagemcnh.php', $baseAutofrotaUrl) ?>"
-                                    target="_self">Condutores com CNH</a></li>
+                                    target="_self">Condutores com CNH</a></li> -->
                             <li><a class="dropdown-item"
                                     href="<?= menuSuperiorLink('condutores/importarcnh.php', $baseAutofrotaUrl) ?>"
                                     target="_self">Importar CNH</a></li>
