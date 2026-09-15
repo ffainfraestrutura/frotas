cdc9b30f40f044f876b39ee51e9452189352478d
Desenvolvimento2FFA <suportedesenvolvimento@ffainfraestrutura.com.br>
2026-09-15 18:15:05 -0300

    feat: atualiza checklist de vistoria

--- checklist/checklistfrota.php
+++ checklist/checklistfrota.php
@@ -114,7 +114,7 @@ header('Content-Type: text/html; charset=utf-8');
         <div class="mt-4 alert alert-warning" role="status">
           <i class="fas fa-exclamation-triangle me-1"></i>
           <strong>Você ainda não tem assinatura.</strong>
-          Por favor, adicione uma em <a class="alert-link" href="/assinaturas">Assinaturas</a>.
+          Por favor, adicione uma em <a class="alert-link" href="/assinaturas" target="_blank" rel="noopener noreferrer">Assinaturas</a>.
         </div>
       <?php else: ?>
         <div class="mt-4 alert alert-secondary" role="status">
